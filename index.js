aa6c4161b5a0d1b815151bb5d70d2eae570f471a
Anuj Kumar Pal <anujkmr974@gmail.com>
2019-04-18 18:18:25 +0530

    Update index.js
    http removed

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 var express=require('express');
 var app=express();
-var http=require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+// var http=require('http').Server(app);
+var server = app.listen(3000);
+var io = require('socket.io').listen(server);
 app.get('/',(req,res)=>{
-
     res.sendFile(__dirname + '/index.html');
-
 });
-
 io.on('connection',function(socket){
     console.log("user connected");
     socket.on('chat message', function(msg){
@@ -16,8 +13,6 @@ io.on('connection',function(socket){
         io.emit('chat message', msg);
       });
 })
-
-
-http.listen(port,()=>{
-    console.log("4000 listening");
-})
+// http.listen(port,()=>{
+//     console.log("4000 listening");
+// })
